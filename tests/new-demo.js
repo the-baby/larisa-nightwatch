cae28eab2577e7129bf352f82dc29644854e6a89
Unknown <larisayablonskaya@gmail.com>
2018-10-31 13:27:45 +0200

    created new file for log in scenario

---
+++ tests/new-demo.js
@@ -0,0 +1,59 @@
+const config = require('config')
+const baseUrl = config.baseUrl
+const creds = config.creds.regularUser
+module.exports = {
+  'ConversationOne' : {
+    'log-in': {
+      before: (browser) => browser.maximizeWindow(),
+      //after: (browser) => browser.end(),
+       'Sign-in successfully leads to the Editor': (browser) => {
+        browser
+        .url(baseUrl + '/user/login')
+        .waitForElementVisible('body', 1000)
+		.setValue('#edit-name', 'Tom@zuznow.com')
+		.click('#edit-submit')
+        .pause(5000)
+        .assert.containsText('.messages.error a', 'Have you forgotten your password')
+		
+	},
+			
+	    'Log in with wrong password should show an error message': (browser) => {
+        browser	
+	   .deleteCookies()
+	   .url(baseUrl + '/user/login')
+	   .waitForElementVisible('body', 1000)
+	   .pause(2000)
+	   .setValue('#edit-name', 'Tom@zuznow.com')
+	   .setValue('#edit-pass', 'Computer1')
+	   .click('#edit-submit')
+	   .pause(5000)
+	   .assert.containsText('.messages.error a', 'Have you forgotten your password')
+	},
+	
+	  'Log in with wrong username should show an error message': (browser) => {
+        browser	
+	   .deleteCookies()
+	   .url(baseUrl + '/user/login')
+	   .waitForElementVisible('body', 1000)
+	   .pause(2000)
+	   .setValue('#edit-name', 'Tim@zuznow.com')
+	   .setValue('#edit-pass', 'Adminuser1')
+	   .click('#edit-submit')
+	   .pause(5000)
+	   .assert.containsText('.messages.error a', 'Have you forgotten your password')
+	},
+	'successful log in of admin user with applications should show editor' : (browser) => {
+        browser	
+	   .deleteCookies()
+	   .url(baseUrl + '/user/login')
+	   .waitForElementVisible('body', 1000)
+	   .pause(2000)
+	   .setValue('#edit-name', 'Tom@zuznow.com')
+	   .setValue('#edit-pass', 'Adminuser1')
+	   .click('#edit-submit')
+	   .pause(5000)
+	   .assert.elementPresent('#menu_new')
+		}
+	}
+  } 
+};
