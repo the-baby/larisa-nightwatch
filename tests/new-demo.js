0b9052e96132e5d690fbbb7e5db3cdd6489006a2
Unknown <larisayablonskaya@gmail.com>
2018-11-05 16:35:24 +0200

    finished

--- tests/new-demo.js
+++ tests/new-demo.js
@@ -19,7 +19,6 @@ module.exports = {
 			
 	    'Log in with wrong password should show an error message': (browser) => {
         browser	
-	   .deleteCookies()
 	   .url(baseUrl + '/user/login')
 	   .waitForElementVisible('body', 1000)
 	   .pause(2000)
@@ -32,7 +31,6 @@ module.exports = {
 	
 	  'Log in with wrong username should show an error message': (browser) => {
         browser	
-	   .deleteCookies()
 	   .url(baseUrl + '/user/login')
 	   .waitForElementVisible('body', 1000)
 	   .pause(2000)
@@ -53,7 +51,33 @@ module.exports = {
 	   .click('#edit-submit')
 	   .pause(5000)
 	   .assert.elementPresent('#menu_new')
-		}
+	},
+	'successful log in of users without applications should show New app screen': (browser) => {
+	  browser	
+	 .deleteCookies()
+	 .url(baseUrl + '/user/login')
+	 .waitForElementVisible('body', 1000)
+	 .pause(2000)
+	 .setValue('#edit-name', 'Polly@zuznow.com')
+	 .setValue('#edit-pass', 'Newuser1')
+	 .click('#edit-submit')
+	 .pause(5000)
+	 .assert.elementPresent('#s2id_industry_select')
+	 	 
+	},
+	'successful log in of non-admin user with applications should show Editor': (browser) => {
+	 browser	
+	 .deleteCookies()
+	 .url(baseUrl + '/user/login')
+	 .waitForElementVisible('body', 1000)
+	 .pause(2000)
+	 .setValue('#edit-name', 'Polly@zuznow.com')
+	 .setValue('#edit-pass', 'Newuser1')
+	 .click('#edit-submit')
+	 .pause(5000)
+	 .assert.elementPresent('#menu_new')
+	
+    }
 	}
   } 
 };
