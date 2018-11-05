4553c75efa49e1da83c9543aa0de00ad2a462b09
Unknown <larisayablonskaya@gmail.com>
2018-11-05 16:35:38 +0200

    created a new scenario for rules sets

---
+++ tests/rules.js
@@ -0,0 +1,70 @@
+const config = require('config')
+const baseUrl = config.baseUrl
+const creds = config.creds.regularUser
+module.exports = {
+  'ConversationOne' : {
+    'faq-generator': {
+      before: (browser) => browser.maximizeWindow(),
+      //after: (browser) => browser.end(),
+       'Sign-in successfully leads to the Editor': (browser) => {
+        browser
+        .url(baseUrl + '/user/login')
+        .waitForElementVisible('body', 1000)
+        .setValue('#edit-name', 'larisa@zuznow.com')
+        .setValue('#edit-pass', 'Lastweek1')
+        .click('#edit-submit')
+        .pause(3000)
+        .assert.visible('#s2id_domain_selection > a > span')
+      },
+	  'Clicking the next button should open the Add sample screen': (browser) => {
+        browser
+		 .click('#menu_new')
+		 .pause(3000)
+		 .click('#s2id_industry_select b')
+		 .click('body > div.select2-drop.select2-drop-active > ul > li:nth-child(6)')
+		 .click('#btnNext')
+		 .pause(4000)
+		 .setValue('#org-name', 'school')
+		 .click('#btnNext')
+		 .pause(2000)
+		 .click('.device.device-chatbot')
+		 .getLocationInView('#btnNext')
+         .assert.visible('#btnNext')
+  		 .click('#btnNext')
+		 .pause(3000)
+		 .assert.visible('.form-control.newIntentSample.first')
+	  },
+	  'Checking that it is possible to add samples to the app': (browser) => {
+        browser
+		.setValue('.form-control.newIntentSample.first', 'How old are you')
+		.getLocationInView('#btnNext')
+		.click('#btnNext')
+		.pause(3000)
+		.assert.visible('.form-control.newTextResponse.first')
+	},
+	'Clicking the Customize button opens the Editor': (browser) => {
+        browser
+	   .setValue('.form-control.newTextResponse.first', 'I am 20')
+	   .pause(3000)
+	   .getLocationInView('#btnNext')
+       .assert.visible('#btnNext')
+  	   .click('#btnNext')
+	   .click('#closeTopMessageBar')
+	   .pause(25000)
+	   .click('.publish-btn.btn.editor')
+	   .assert.elementPresent('#btnSave')
+		
+		
+	},
+	
+	'Clicking the Plus button adds a rules set': (browser) => {
+       browser
+	  .pause(8000)
+	  .click('#btnAddRulesSet')
+	  .pause(10000)
+	  .assert.visible('.setName[set="1"]')
+	  
+	}
+  }
+}
+}  
